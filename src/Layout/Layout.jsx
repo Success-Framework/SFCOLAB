4e1409c8481d61dde62ba3a1710f786229429079
unknown <ejeanobionyeka@gmail.com>
2025-08-04 04:58:41 +0100

    fixed overflow on the dashboard page

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import { Outlet, useLocation } from 'react-router-dom'
-import NavBar from '../components/sections/NavBar'
-import SideBar from '../components/sections/SideBar'
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import NavBar from "../components/sections/NavBar";
+import SideBar from "../components/sections/SideBar";
 
 const Layout = () => {
-    const location = useLocation();
-    const isChatPage = location.pathname === '/messages';
-    return (
-        <div className="h-screen w-screen  bg-[#000000]">
-            <div className='w-full h-[10%]'>
-                <NavBar />
-            </div>
-            {isChatPage ? (
-                <div className='w-full h-[90%]'>
-                    <Outlet />
-                </div>
-            ) : (
-                <div className='relative h-[90%] w-full  flex'>
-                    <SideBar />
-                    <div className='text-white flex flex-col w-full  p-5 gap-10'>
-                        <div className='w-full h-[100%] pt-3.5 overflow-y-auto'>
-                            <Outlet />
-                        </div>
-                    </div>
-                </div>
-            )}
+  const location = useLocation();
+  const isChatPage = location.pathname === "/messages";
+  return (
+    <div className="h-screen w-screen bg-[#000000] overflow-hidden">
+      <div className="w-full h-[10%]">
+        <NavBar />
+      </div>
+      {isChatPage ? (
+        <div className="w-full h-[90%] overflow-hidden">
+          <Outlet />
         </div>
-    )
-}
+      ) : (
+        <div className="relative h-[90%] w-full flex overflow-hidden">
+          <SideBar />
+          <div className="text-white flex flex-col w-full p-5 gap-10 overflow-hidden">
+            <div className="w-full h-[100%] pt-3.5 overflow-y-auto overflow-x-hidden">
+              <Outlet />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
--- src/components/pages/dashboard.jsx
+++ src/components/pages/dashboard.jsx
@@ -1,24 +1,20 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import WorldClock from "../sections/WorldClock"
-import Calendar from "../sections/Calendar"
-import Tasks from "../sections/Tasks"
-import DashboardHeader from "../headers/DashboardHeader"
-import DashboardSection from "../sections/DashboardSection"
-import TaskProgress from "../sections/TaskProgress"
+import React, { useState } from "react";
+import WorldClock from "../sections/WorldClock";
+import Calendar from "../sections/Calendar";
+import Tasks from "../sections/Tasks";
+import DashboardHeader from "../headers/DashboardHeader";
+import DashboardSection from "../sections/DashboardSection";
+import TaskProgress from "../sections/TaskProgress";
 
 const Dashboard = () => {
-  
-
   return (
-    <div className="min-h-screen bg-black text-white">
-
-    <DashboardHeader/>     
-
+    <div className="min-h-screen bg-black text-white w-full overflow-x-hidden">
+      <DashboardHeader />
 
       {/* Main Content */}
-      <div className="w-full mx-auto py-4 mb-4">
+      <div className="w-full mx-auto py-4 mb-4 overflow-x-hidden">
         <div className="flex flex-col lg:flex-row gap-6 w-full">
           <div className="w-full h-full overflow-y-auto lg:w-1/3">
             <WorldClock />
@@ -28,16 +24,19 @@ const Dashboard = () => {
           </div>
           <div className="w-full h-full overflow-y-auto lg:w-1/3">
             {/* <Tasks /> */}
-            <TaskProgress/>
+            <TaskProgress />
           </div>
         </div>
       </div>
-      <Tasks />   
+      <div className="w-full overflow-x-hidden">
+        <Tasks />
+      </div>
 
-      <DashboardSection/>
-      
+      <div className="w-full overflow-x-hidden">
+        <DashboardSection />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard 
+export default Dashboard;
